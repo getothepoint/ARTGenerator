5a7d06bb1722465df430bac2249f1eef9ed10d6e
getothepoint <carlossantana9067@gmail.com>
2025-07-01 12:48:10 -0400

    Initial Commit

---
+++ app/utils.py
@@ -0,0 +1,20 @@
+TOP_IMAGE_MODELS = [
+    "stable_diffusion",       # Classic, versatile general model
+    "deliberate_v2",          # High quality, detailed outputs
+    "dreamshaper",            # Creative, stylized artistic outputs
+    "realistic_vision",       # Photorealistic results, great for portraits
+    "absolute_reality",       # Hyper-realistic image generation
+    "openjourney",            # MidJourney-like style
+    "pastel_mix",             # Soft colors and pastel style
+    "f222",                  # Popular anime style model
+    "easynegative",           # Specialized in better negative prompting
+    "anythingv3"              # Versatile anime-style model
+]
+
+valid_sizes = [
+    "512x512",      # balanced square
+    "768x768",      # high-res square
+    "512x768",      # portrait
+    "768x512",      # landscape
+    "1024x1024"     # high-res (may take longer) 
+]
