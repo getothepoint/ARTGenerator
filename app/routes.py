ca1f8d5a3b023e3cb42b0487908fb421749d303c
getothepoint <carlossantana9067@gmail.com>
2025-07-10 17:04:12 -0400

    Added the denoising strength to improve img2img output

--- app/routes.py
+++ app/routes.py
@@ -20,45 +20,39 @@ async def read_root():
 async def generate_image(prompt: str = Form(...), 
                          context_image: Optional[UploadFile] = File(None),
                          steps: int = 40,
+                         denoising_strength: float = .05,
                          image_size: str = Query("512x512", description = "Select the image size", enum=utils.valid_sizes), 
                          model: str = Query("stable_diffusion", description= "Choose the image generation model", enum= utils.TOP_IMAGE_MODELS)):
     if not prompt.strip(): 
         raise HTTPException(status_code=400, detail = "Please include a description of what you would like to do" )
-    base64_string: Optional[str] = None
     if context_image:
-       print("Image:", context_image)
        image_data = await context_image.read()
        try:
             image = Image.open(BytesIO(image_data))
             format = image.format
-            base64_string = base64.b64encode(image_data).decode('utf-8')  
             if format.upper() not in ["PNG", "JPEG", "JPG"]:
                 raise HTTPException(status_code = 400, detail= "Only PNG and JPEG files are allowed.")
        except:
             raise HTTPException(status_code=400, detail= " This is not a valid image file")
-    payload = {
-                "prompt": prompt,
-                "context_image": base64_string}
-    request_id = await submit_to_stable_horde(prompt, model, image_size, steps)  # Just an example to uniquely identify the request
+    request_id = await submit_to_stable_horde(prompt, model, image_size, steps, denoising_strength)  # Just an example to uniquely identify the request
     return JSONResponse(
         content={
         "message": "Your image is being processed. Check back soon.",
         "request_id": request_id
         },
         status_code=202 ) 
-async def generate_image_from_prompt(prompt: str, image_size: str = "512x512", context_image: Optional[str] = None, model: str = "stable_diffusion"):
-    if model not in utils.TOP_IMAGE_MODELS:
-        raise HTTPException(status_code=400, detail= "This is a model that is not support on the list. Please choose another model.")
-    if image_size not in utils.valid_sizes:
-        raise HTTPException(status_code=400, detail = "Invalid image size. Choose from 256x256, 512x512, or 1024x1024.") 
-async def submit_to_stable_horde(prompt: str, model: str, image_size: str, steps:int, context_image: Optional[str] = None):
+async def submit_to_stable_horde(prompt: str, model: str, image_size: str, steps:int, denoising_strength=0.05, context_image: Optional[str] = None):
     size_parts = image_size.split("x")
     width, height = map(int, size_parts)
     params = {"model": model,
-              "n": 10,
+              "n": 1,
               "width": width,
               "height": height,
-              "steps": steps}
+              "steps": steps,
+              }
+    if context_image: 
+        params["context_image"] = context_image
+        params["denoising_strength"]= denoising_strength
     payload = {"prompt": prompt,
                "params": params
                }
